00206ad08c2f86dab67c121f346342b0cba94895
John Zoeller <johnzoeller@google.com>
2025-07-14 14:58:54 -0400

    Migrate Wear sample to a module in the mobile directory

---
+++ Shrine/wear/src/main/java/com/authentication/shrinewear/extensions/LifecycleAwait.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.authentication.shrinewear.extensions
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+/**
+ * Suspends the coroutine until the Lifecycle reaches a desired state.
+ *
+ * @param state The target state to wait for.
+ */
+suspend fun Lifecycle.awaitState(state: Lifecycle.State) {
+    if (currentState.isAtLeast(state)) {
+        return
+    }
+
+    suspendCancellableCoroutine { continuation ->
+        val observer = object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                if (source.lifecycle.currentState.isAtLeast(state)) {
+                    if (continuation.isActive) {
+                        continuation.resume(Unit)
+                    }
+                    source.lifecycle.removeObserver(this)
+                } else if (event == Lifecycle.Event.ON_DESTROY) {
+                    continuation.cancel()
+                    source.lifecycle.removeObserver(this)
+                }
+            }
+        }
+
+        addObserver(observer)
+        continuation.invokeOnCancellation {
+            removeObserver(observer)
+        }
+    }
+}
